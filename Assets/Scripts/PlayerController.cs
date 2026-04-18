f8a0da4bfd463688db618facdf6a64211ce885dc
gretanakub <gretanakub@gmail.com>
2026-04-18 22:41:10 +0700

    playercontrolrotate

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,12 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float rotateSpeed = 10f; 
 
     private CharacterController controller;
     private Vector2 moveInput;
     private Vector3 velocity;
 
-    // Start is called once before the first execution of Update after the
-    // MonoBehaviour is created
     void Awake()
     {
         controller = GetComponent<CharacterController>();
@@ -21,11 +20,15 @@ public class PlayerController : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
         controller.Move(move * speed * Time.deltaTime);
+
+        if (move != Vector3.zero)
+        {
+            transform.forward = Vector3.Slerp(transform.forward, move, Time.deltaTime * rotateSpeed);
+        }
 
         velocity.y += Physics.gravity.y * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
