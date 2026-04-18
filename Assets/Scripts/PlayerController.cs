6ffea4327a276572cb26a9ce68aab36e8115e3ab
gretanakub <gretanakub@gmail.com>
2026-04-18 22:51:09 +0700

    pscon

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,9 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-    [SerializeField] private float rotateSpeed = 10f; 
+    [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private float playerHeight = 2f;
+    [SerializeField] private float playerRadius = 0.5f;
 
     private CharacterController controller;
     private Vector2 moveInput;
@@ -22,12 +24,65 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
-        controller.Move(move * speed * Time.deltaTime);
+        Vector3 moveDir = new Vector3(moveInput.x, 0, moveInput.y);
+        float moveDistance = speed * Time.deltaTime;
 
-        if (move != Vector3.zero)
+        
+        bool canMove = !Physics.CapsuleCast(
+            transform.position,
+            transform.position + Vector3.up * playerHeight,
+            playerRadius,
+            moveDir,
+            moveDistance
+        );
+
+        if (!canMove)
         {
-            transform.forward = Vector3.Slerp(transform.forward, move, Time.deltaTime * rotateSpeed);
+           
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(
+                transform.position,
+                transform.position + Vector3.up * playerHeight,
+                playerRadius,
+                moveDirX,
+                moveDistance
+            );
+
+            if (canMove)
+            {
+                
+                moveDir = moveDirX;
+            }
+            else
+            {
+                
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(
+                    transform.position,
+                    transform.position + Vector3.up * playerHeight,
+                    playerRadius,
+                    moveDirZ,
+                    moveDistance
+                );
+
+                if (canMove)
+                {
+                   
+                    moveDir = moveDirZ;
+                }
+                
+            }
+        }
+
+        if (canMove)
+        {
+            controller.Move(moveDir * moveDistance);
+        }
+
+       
+        if (moveDir != Vector3.zero)
+        {
+            transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
         }
 
         velocity.y += Physics.gravity.y * Time.deltaTime;
