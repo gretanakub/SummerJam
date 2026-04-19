b6889c12f22e0d800340a484abc108c065764e18
SorawichLM <Sorawichkeawkham@gmail.com>
2026-04-20 00:18:46 +0700

    Uifinal

---
+++ Assets/FirstFolder/OderUi/Icon/Test.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private OrderManager orderManager;
+
+    void Start()
+    {
+        // 1.Citrus Sunrise Blend
+        // 2.Berry Tropical Rush
+        // 3.Golden Pine Citrus
+        // 4.Melon Sweet Splash
+        // 5.Berry Citrus Punch
+        // 6.Tropical Wave Juice
+        // 7.Sunset Berry Lemon
+        // 8.Orange Melon Chill
+
+        orderManager = FindObjectOfType<OrderManager>();
+        orderManager.SpawnOrder("Berry Citrus Punch");          // 45 วิ
+        orderManager.SpawnOrder("Berry Citrus Punch", 30f);     // 30 วิ
+        orderManager.SpawnOrder("Orange Melon Chill");                 // 45 วิ
+
+        orderManager.CompleteOrder("Berry Citrus Punch");       // ลบออก (ทำสำเร็จ)
+    }
+}
